a8aba9c8259351526ccca71c643b5f38a1a18fd3
Andrea <ironhack@Ironhacks-MacBook-Pro-2.local>
2023-02-13 21:37:18 +0100

    Principio de nav

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 
 router.get('/', (req, res, next) => res.render('home'))
 
-router.get('/signup', authController.signup);
-router.post('/signup', authController.doSignup);
+router.get('/signup', authMiddleware.isNotAuthenticated, authController.signup);
+router.post('/signup', authMiddleware.isNotAuthenticated, authController.doSignup);
 
-router.get('/login', authController.login);
-router.post('/login', authController.doLogin);
+router.get('/login', authMiddleware.isNotAuthenticated, authController.login);
+router.post('/login', authMiddleware.isNotAuthenticated, authController.doLogin);
+
+//router.get('/logout', authMiddleware.isAuthenticated, authController.doLogout);
 
 router.get('/home', authController.home)
+
+
 
 module.exports = router;
