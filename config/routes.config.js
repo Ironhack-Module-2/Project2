04a6239da7a4f86aabdd19a0a15e925ccc94e6ed
elena992 <elena.fernandez1992@gmail.com>
2023-02-18 14:31:39 +0100

    home del artista creada

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,26 +1,46 @@
-const router = require('express').Router();
-const authController = require('../controllers/auth.controller');
-const roleController = require('../controllers/role.controller');
-const createController = require('../controllers/create.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
+const router = require("express").Router();
+const authController = require("../controllers/auth.controller");
+const roleController = require("../controllers/role.controller");
+const createController = require("../controllers/create.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 //const roleMiddleware = require('../middlewares/role.middleware');
 
+router.get("/", (req, res, next) => res.render("home"));
 
-router.get('/', (req, res, next) => res.render('home'))
+router.get("/signup", authMiddleware.isNotAuthenticated, authController.signup);
+router.post(
+  "/signup",
+  authMiddleware.isNotAuthenticated,
+  authController.doSignup
+);
 
-router.get('/signup', authMiddleware.isNotAuthenticated, authController.signup);
-router.post('/signup', authMiddleware.isNotAuthenticated, authController.doSignup);
+router.get("/login", authMiddleware.isNotAuthenticated, authController.login);
+router.post(
+  "/login",
+  authMiddleware.isNotAuthenticated,
+  authController.doLogin
+);
 
-router.get('/login', authMiddleware.isNotAuthenticated, authController.login);
-router.post('/login', authMiddleware.isNotAuthenticated, authController.doLogin);
+router.get("/logout", authMiddleware.isAuthenticated, authController.doLogout);
 
-router.get('/logout', authMiddleware.isAuthenticated, authController.doLogout);
+router.get("/home", authMiddleware.isAuthenticated, roleController.home);
 
-router.get('/home', authMiddleware.isAuthenticated, roleController.home);
+router.get("/create", authMiddleware.isAuthenticated, createController.create);
+router.post(
+  "/create",
+  authMiddleware.isAuthenticated,
+  createController.doCreate
+);
 
-router.get('/create', authMiddleware.isAuthenticated, createController.create);
-router.post('/create', authMiddleware.isAuthenticated, createController.doCreate);
-
-
+router.get(
+  "/profile-set",
+  authMiddleware.isAuthenticated,
+  roleController.profile
+);
+router.get(
+  "/home-artist",
+  authMiddleware.isAuthenticated,
+  roleController.homeArtist
+);
 
 module.exports = router;
--- middlewares/role.middleware.js
+++ middlewares/role.middleware.js
@@ -5,3 +5,11 @@ module.exports.isHunter = (req, res, next) => {
     res.redirect('/profile') //view de 'no eres hunter'
   }
 }
+
+module.exports.isArtist = (req, res, next) => {
+  if (req.user.role === 'artist') {
+    next()
+  } else {
+    res.redirect('/profile-set') //view de 'no eres artist'
+  }
+}
