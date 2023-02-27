ccc25a6745aaaed0ffb71811279efacc0692f9a3
Andrea <ironhack@Ironhacks-MacBook-Pro-2.local>
2023-02-27 16:57:39 +0100

    listo

--- config/db.config.js
+++ config/db.config.js
@@ -1,21 +1,27 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', true)
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/express-tweethack'
+const MONGO_URL =
+  process.env.MONGO_URL ||
+  "mongodb+srv://avmancera28:ThZBLWrJewgpRQZP@cluster0.cxkjo90.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  autoIndex: true, //make this also true
-})
-  .then(response => console.log(`Connected to Mongo! Database name: '${response.connections[0].name}'`))
-  .catch(err => console.error('Error connecting to mongo', err))
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    autoIndex: true, //make this also true
+  })
+  .then((response) =>
+    console.log(
+      `Connected to Mongo! Database name: '${response.connections[0].name}'`
+    )
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
-process.on('SIGINT', function () {
+process.on("SIGINT", function () {
   mongoose.connection.close(function () {
-    console.log('Mongoose disconnected on app termination');
+    console.log("Mongoose disconnected on app termination");
     process.exit(0);
   });
 });
 
-
-module.exports.MONGO_URL = MONGO_URL
+module.exports.MONGO_URL = MONGO_URL;
